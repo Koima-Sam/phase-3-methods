3e13a6aebde1ae2e1edde0f74b20d97d43e6139a
Koima-Sam <sam17koima@gmail.com>
2022-08-17 17:02:05 +0300

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+#greet programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# greet a person
+def greet name
+    puts "Hello, #{name}!"
+end
+
+# greet_with_default
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# add two numbers
+def add a,b
+    a+b
+end
+
+# half the given int
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    return num/2
+end
+
+
